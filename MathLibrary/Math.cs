175c0ea33c39579abbf6db5a5b7ed9b0c7a26553
Pete Mathews <petemathews14@gmail.com>
2021-02-16 12:35:22 -0500

    Pre Debug

---
+++ MathLibrary/Math.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace MathLibrary {
+	class Math {
+
+		public static int Add(int a, int b) {
+			return a + b;
+		}
+
+		public static int Subtract(int a, int b) {
+			return a - b;
+		}
+
+		public static int Multiply(int a, int b) {
+			return a * b;
+		}
+
+		public static int Divide(int a, int b) {
+			return a / b;
+		}
+
+
+
+
+		// Create the modulo method.
+		// Takes two integers, divides the second into the first and returns the remainder of the integer division.
+
+		public static int Mod(int a, int b) {
+			return a % b;
+		}
+
+		public static int Modulo(int a, int b) {
+			return a - (a / b * b);
+		}
+
+
+		/*
+				Create a method that takes two integers and raises the first integer to the power of the second integer.
+
+					Power(2, 3) = 2*2*2
+					Power(5, 4) = 5*5*5*5
+		*/
+
+		public static int Power(int a, int b) {
+
+
+			var x = (b == 0) ? 0 : a;
+			for (var idx = 1; idx < b; idx++) {
+				x *= a;
+			};
+			return x;
+
+			/*
+			
+
+			
+			 */
+
+
+
+
+
+			/* 
+			// Doug's Way
+			var answer = 1;
+			for (var idx = 0; idx < b; idx++) {
+				answer *= a;
+			};
+			return answer;
+
+			// Fails when b == 0
+			*/
+
+		}
+
+
+
+	}
+}
---
+++ MathLibrary/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MathLibrary {
+	class Program {
+		static void Main(string[] args) {
+
+			// var math = new Math(); // Do not need to 
+			var answer = Math.Add(1, 2);
+			Console.WriteLine($"{answer}\n");
+
+			var answerS = Math.Subtract(1, 2);
+			Console.WriteLine($"{answerS}\n");
+
+			var answerM = Math.Multiply(6, 2);
+			Console.WriteLine($"{answerM}\n");
+
+			var answerD = Math.Divide(9, 3);
+			Console.WriteLine($"{answerD}\n");
+
+			var answerO1 = Math.Mod(7, 2);
+			Console.WriteLine($"{answerO1}\n");
+
+			var answerO2 = Math.Mod(13, 5);
+			Console.WriteLine($"{answerO2}\n");
+
+			var answerO3 = Math.Mod(1234, 13);
+			Console.WriteLine($"{answerO3}\n");
+
+
+
+
+			var answer1 = Math.Power(2, 2);
+			Console.WriteLine($"pow {answer1}\n");
+
+			var answer2 = Math.Power(5, 5);
+			Console.WriteLine($"{answer2}\n");
+
+			var answer3 = Math.Power(2, 0);
+			Console.WriteLine($"{answer3}\n");
+
+			/*
+
+			var answer1 = Math.Modulo(7, 2);
+			Console.WriteLine($"{answer1}\n");
+
+			var answer2 = Math.Modulo(13, 5);
+			Console.WriteLine($"{answer2}\n");
+
+			var answer3 = Math.Modulo(1234, 13);
+			Console.WriteLine($"{answer3}\n");
+
+
+			*/
+
+		}
+	}
+}
